bc00db44d8109d19dafa93203bf6336421d4d076
Ashutosh Kumar <kashslash7@gmail.com>
2022-11-20 10:36:02 +0530

    updated get started button on home page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 const Content = () => {
     return(
         <div className="container mx-auto pb-9">
@@ -5,7 +6,7 @@ const Content = () => {
             <span className="flex justify-center text-center px-56 py-4">The Delhi Commission for Protection of Child Rights has attempted to create extensive repository of all existing entitlements and relevant information which could be of benefit to Persons with Disabilities & their caregivers in Delhi. This website aims to address the information asymmetry by creating awareness about the rights of Persons with Disabilities, especially Children with Special Needs or Children who have parents with disabilities & assist them in understanding the means of leveraging rightful entitlements for their own benefit.</span>
             <span className="flex justify-center text-center px-16 py-4">Click to know your rights & entitlement</span>
             <div className="flex justify-center">
-                <span className="box-border rounded bg-black text-white px-16 py-2">Get Started</span>
+                <button className="box-border rounded bg-black text-white px-16 py-2"><Link to="/FindTheService">Get Started</Link></button>
             </div>
             
         </div>
